5d1ecc84b80ea7c046e99c38b770722659854dad
magusin <fanatsy4@gmail.com>
2022-03-11 17:20:46 +0100

    pagination

--- index.php
+++ index.php
@@ -7,30 +7,58 @@ $users = $connection
     ->query($sql)
     ->fetchAll(PDO::FETCH_ASSOC);
 
-$sql2 = 'SELECT * FROM `product` ORDER BY `id` DESC LIMIT 5';
-$products = $connection
-    ->query($sql2)
-    ->fetchAll(PDO::FETCH_ASSOC);
-/**
- * @todo Edit user
- * @todo Delete User
- *
- * @todo Pagination
- *
+// $sql2 = 'SELECT * FROM `product` ORDER BY `id`';
+// $products = $connection
+//     ->query($sql2)
+//     ->fetchAll(PDO::FETCH_ASSOC);
 
-* @todo TP M'afficher les 5 derniers produits sur la page d'accueil (id, name, selling_price)
-* @todo Je veux pouvoir cliquer sur un produit et avoir le détail du produit
-* @todo               (name, cost_price, selling_price, category) (sur une nouvelle page)
-*/
-?>
-?>
+// on détermine sur quel page on se trouve
+if(isset($_GET['page']) && !empty($_GET['page'])){
+    $currentPage = (int) strip_tags($_GET['page']);
+}else{
+    $currentPage = 1;
+}
 
-<?php
+// On détermine le nombre total d'articles
+$sql3 = 'SELECT COUNT(*) AS `id` FROM `product`';
+
+// On prépare la requête
+$query = $connection->prepare($sql3);
+
+// On exécute
+$query->execute();
+
+// On récupère le nombre de produit
+$result = $query->fetch();
+
+$nbProduct = (int) $result['id'];
+
+// On détermine le nombre de produit par page
+$parPage = 5;
+
+// On calcule le nombre de pages total
+$pages = ceil($nbProduct / $parPage);
+
+// Calcul du 1er article de la page
+$premier = ($currentPage * $parPage) - $parPage;
+
+$sql4 = 'SELECT * FROM `product` ORDER BY `id` DESC LIMIT :premier, :parpage;';
+
+// On prépare la requête
+$query = $connection->prepare($sql4);
+
+$query->bindValue(':premier', $premier, PDO::PARAM_INT);
+$query->bindValue(':parpage', $parPage, PDO::PARAM_INT);
+
+// On exécute
+$query->execute();
+
+// On récupère les valeurs dans un tableau associatif
+$products = $query->fetchAll(PDO::FETCH_ASSOC);
 require_once 'template_head.php';
 ?>
 
 <h3>Users</h3>
-
 <table>
     <thead>
     <tr>
@@ -79,5 +107,23 @@ require_once 'template_head.php';
     <?php endforeach; ?>
     </tbody>
 </table>
+<nav>
+    <ul class="pagination">
+        <!-- Lien vers la page précédente (désactivé si on se trouve sur la 1ère page) -->
+            <li class="page-item <?= ($currentPage == 1) ? "disabled" : "" ?>">
+                <a href="./?page=<?= $currentPage - 1 ?>" class="page-link">Précédente</a>
+            </li>
+            <?php for($page = 1; $page <= $pages; $page++): ?>
+                <!-- Lien vers chacune des pages (activé si on se trouve sur la page correspondante) -->
+            <li class="page-item <?= ($currentPage == $page) ? "active" : "" ?>">
+                <a href="./?page=<?= $page ?>" class="page-link"><?= $page ?></a>
+            </li>
+            <?php endfor ?>
+            <!-- Lien vers la page suivante (désactivé si on se trouve sur la dernière page) -->
+            <li class="page-item <?= ($currentPage == $pages) ? "disabled" : "" ?>">
+                <a href="./?page=<?= $currentPage + 1 ?>" class="page-link">Suivante</a>
+            </li>
+    </ul>
+</nav>
 </body>
 </html>
